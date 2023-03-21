09277b05e421fe931d6a04b9eeb8311fd1077679
jbakela <jbakela@uwo.ca>
2023-03-21 11:56:39 -0400

    reset selected value for Team select

--- js/5K24SPARK.js
+++ js/5K24SPARK.js
@@ -273,7 +273,6 @@ function newMatch(){
     $('#nameSelect').val(name);
     $('#nameSelect').trigger('change');
 
-    $("#teamSelect").val('').trigger('change')
     // Change back to pre-match screen
     document.getElementById("Name").innerHTML = "Pre-Match";
     document.getElementById(0).style.display = "table";
